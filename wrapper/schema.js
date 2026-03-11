b2d1ed13e73b0580ef0c52a9e2122c68ebdee0b4
LokoKanishka <loko.kanishka@gmail.com>
2026-03-11 03:18:28 -0300

    chore: update schema allowed settings and ignore persistent memory data

--- wrapper/schema.js
+++ wrapper/schema.js
@@ -52,7 +52,11 @@ const Schemas = {
 
     settingsUpdate: (input) => {
         // Enforce safe updates only (MVP risk reduction)
-        const allowedSafeSettings = ['temp', 'api_server_default', 'public_api', 'force_scroll'];
+        const allowedSafeSettings = [
+            'temp', 'api_server_default', 'public_api', 'force_scroll',
+            'api_preset_openai', 'api_preset_kobold', 'api_preset_oobabooga',
+            'api_preset_ollama', 'api_preset_anthropic', 'api_preset_openrouter'
+        ];
         const updates = {};
         
         for (const key of allowedSafeSettings) {
